1b2f05386625166837c0aeb365dc9407f3de0cac
DDSCentral <dds@ddscentral.org>
2018-06-21 20:09:40 +0300

    Add menu system generation.

--- index.php
+++ index.php
@@ -10,10 +10,35 @@ require_once(__DIR__ . "/functions.php");
 
 $conn = connect_db();
 
+function generate_menu_html($conn, $parent_id = 0, $lvl = 0, $base="") {
+    $items = get_menus_by_parent_id($conn, $parent_id);
+
+    if (count($items) == 0) {
+        return;
+    }
+
+    echo "<ul class='navigate'>";
+        foreach ($items as $id => $item) {
+            echo "<li class='level" . $lvl . "'><a href='" . $base . $item->url . "'>" . $item->label . "</a></li>";
+
+            $new_base = $base . $item->url . '/';
+
+            generate_menu_html($conn, $id, $lvl++, $new_base);
+        }
+    echo "</ul>";
+
+
+}
+
 if ($conn->connect_error) {
     die("Connection failed : " . $conn->connect_error);
 }
 
-$ids = get_menus_by_parent_id($conn, 0);
+echo "<!DOCTYPE html>\n";
+echo "<head><title>Links</title></head>\n";
+echo "<body>\n";
 
-print_r($ids);
+generate_menu_html($conn);
+
+echo "</body>\n";
+echo "</html>\n";
